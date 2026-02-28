4eaef5098d33ab8eeca1b7be343e0772bea04864
Jakha Cham <you@example.com>
2026-02-28 16:33:30 -0500

    hw4

---
+++ gremlins.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main( int argc, char *argv[] ) {
+ char fileName[100];
+ FILE *filep;
+ int c; 
+
+ // displaysyntaxifnoargumentspassedoncommandline
+    if ( argc <= 1 ) {
+        printf("Syntax: %s filename\n", argv[0] );
+        return 1;
+    }
+
+    //copy filename 
+    // if an argument is passed, it must be a file name
+    strcpy(fileName, argv[1]);
+
+    //open file 
+    filep = fopen(fileName, "r");
+    //just in case 
+    if ( filep == NULL ) {
+        printf("Error opening file %s\n", fileName);
+        return 1;
+    }
+
+    //read file letter by letter to change each g to gremlin 
+    while ( (c = fgetc(filep)) != EOF ) {
+        if ( c == 'g' ) {
+            printf("gremlin");
+        }else if ( c == 'G' ) {
+            printf("Gremlin");
+        } else {
+            putchar(c);
+        }
+    }
+    fclose(filep);
+    return 0;
+} 
