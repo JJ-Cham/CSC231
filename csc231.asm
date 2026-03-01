db91fdcf9a7deaf13167febe394e944e3dab6f43
Jakha Cham <you@example.com>
2026-02-28 21:10:06 -0500

    fixing

--- csc231.asm
+++ csc231.asm
@@ -3,49 +3,29 @@
 ;;;
 ;;; Prints the CSC231 bubble letters to stdout
 ;;;
-;;; to assemble and run:
-;;;
-;;;     nasm-f elf csc231.asm
-;;;     ld-melf_i386-o csc231 csc231.o
-;;;     ./csc231
-;;; -------------------------------------------------------------------
 
-;%include files here...
+section .data
 
+msg:    db "____ ____   ____   ____  _____ _", 10
+        db "/ ___/ ___| / ___| |___ \|___ // |", 10
+        db "| | \___ \| |       __) | |_ \| |", 10
+        db "| |___ ___) | |___   / __/ ___) | |", 10
+        db "\____|____/ \____| |_____|____/|_|", 10
+len     equ $ - msg
 
-
-        ;; ------------------------------------------------------------
-        ;; data areas
-        ;; ------------------------------------------------------------
-
-        section .data
-
-msg:    db  '____ ____   ____   ____  _____ _', 10
-        db  '/ ___/ ___| / ___| |___ \\|___// |', 10
-        db  '| |   \\___ \\| |       __) | |_ \\| |', 10
-        db  '| |___ ___) | |___   / __/___)  | |', 10
-        db  '\\____|____/ \\____| |_____|____/|_|', 10
-len:    equ $ - msg
-
-        ;; ------------------------------------------------------------
-        ;; code area
-        ;; ------------------------------------------------------------
-
-        section .text
-        global  _start
+section .text
+global _start
 
 _start:
 
-
-        ;; (add your code here!!!!)
         ; write(1, msg, len)
-        mov     rax, 1          ; sys_write
-        mov     rdi, 1          ; fd = stdout
-        mov     rsi, msg        ; buffer
-        mov     rdx, len        ; length
-        syscall
+        mov     eax, 4          ; sys_write
+        mov     ebx, 1          ; stdout
+        mov     ecx, msg        ; address
+        mov     edx, len        ; length
+        int     0x80
 
         ; exit(0)
-        mov     rax, 60         ; sys_exit
-        xor     rdi, rdi        ; status = 0
-        syscall
+        mov     eax, 1
+        xor     ebx, ebx
+        int     0x80
